23cfc516acdb596e045111bd648dd758e2483ea5
Belinda Ford <belindamford@gmail.com>
2024-12-05 13:15:15 +1100

    Added PBS folder for Streamlit deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python your_dash_script.py
---
+++ Script.js
@@ -0,0 +1,3 @@
+C:/Users/belin/PycharmProjects/Population Health/PBS/GCCSA_2021.shp
+C:/Users/belin/PycharmProjects/Population Health/PBS/GCCSA_2021.shx
+C:/Users/belin/PycharmProjects/Population Health/PBS/GCCSA_2021.dbf
